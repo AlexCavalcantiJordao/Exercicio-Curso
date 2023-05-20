48e200601234630ce9d7824f2e19d29ff34a75b6
AlexCavalcantiJordao <alex37705732@gmail.com>
2023-05-20 15:00:39 -0300

    Exercicio-Vetor.cpp Lista-Vetores.dev

---
+++ Exercicio-Vetor.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int vetor[5] = {1, 2, 3, 4, 5};
+
+  for(int i = 0; i < 5; i++) {
+    cout << vetor[i] << endl;
+  }
+
+  return 0;
+}
